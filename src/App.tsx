675e8f1b95690b6bd95a0c73f69f07eeeeec4591
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-15 22:20:29 +0100

    use the wrapper component and apply .map to the data

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,15 @@
+import cards from './assets/data';
 import Card from './components/Card';
-const singleData = {
-  imgSrc:
-    'https://images.unsplash.com/photo-1661869535393-872dea2d9f8d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80',
-  name: 'Adam',
-  surname: 'Jochemczyk',
-  street: 'TestStreet',
-  postCode: '00-123',
-  town: 'Town',
-  subRegion: 'Region',
-  phoneNumber: '+48 123 456 789',
-};
+import Wrapper from './components/Wrapper';
 
 export default function App() {
   return (
     <main className="grid min-h-screen place-items-center bg-slate-900 px-4">
-      <Card data={singleData} />
+      <Wrapper>
+        {cards.map((card) => (
+          <Card data={card} key={card.id} />
+        ))}
+      </Wrapper>
     </main>
   );
 }
