d72920ca1068615fac0a90091c683d880e34f193
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-16 11:43:54 +0100

    add several things in different files to complete task 4

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import cards from './assets/data';
-import Card from './components/Card';
-import Wrapper from './components/Wrapper';
+import cards from "./assets/dummy-data/data";
+import Card from "./components/Card";
+import Wrapper from "./components/Wrapper";
+import AsideMenu from "./components/AsideMenu";
 
 export default function App() {
   return (
-    <main className="grid min-h-screen place-items-center bg-slate-900 px-4">
+    <main className="grid grid-cols-[100px_1fr] min-h-screen place-items-center bg-slate-900">
+      <AsideMenu />
       <Wrapper>
         {cards.map((card) => (
           <Card data={card} key={card.id} />
