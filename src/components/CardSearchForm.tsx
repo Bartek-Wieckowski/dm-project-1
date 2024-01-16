1579d037c9ed551b70e18403313d86256709128b
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-16 22:40:48 +0100

    merge branch after complete task 1 of hooks block

---
+++ src/components/CardSearchForm.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+interface CardSearchFormProps {
+  onSearchName: (value: string) => void;
+}
+
+export default function CardSearchForm({ onSearchName }: CardSearchFormProps) {
+  function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+  }
+
+  function handleInputChange(e: React.ChangeEvent<HTMLInputElement>) {
+    const value = e.target.value;
+    onSearchName(value);
+  }
+
+  return (
+    <form className="mb-4 pl-2" onSubmit={handleSubmit}>
+      <input
+        placeholder="Wpisz imię..."
+        className="w-28 rounded-lg bg-slate-400 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-900 focus:outline-none focus:ring focus:ring-slate-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72"
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+}
