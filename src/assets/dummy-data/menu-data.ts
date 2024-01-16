f34b8676b1bba95d9ddab4f0f886655dc0a97912
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-16 11:40:36 +0100

    create folder to keep all files with dummy data

---
+++ src/assets/dummy-data/menu-data.ts
@@ -0,0 +1,34 @@
+//FIXME: do omówienia
+
+// import { MenuItem } from "../../types/MenuItem.type";
+// import {
+//   BsFillPeopleFill,
+//   BsFillBagCheckFill,
+//   BsFillFileEarmarkRuledFill,
+//   BsFillHddRackFill,
+// } from "react-icons/bs";
+
+// const menuData: MenuItem[] = [
+//   {
+//     linkName: "Clients",
+//     link: "/clients",
+//     icon: (<BsFillPeopleFill />) as IconType,
+//   },
+//   {
+//     linkName: "Orders",
+//     link: "/orders",
+//     icon: (<BsFillBagCheckFill />) as IconType,
+//   },
+//   {
+//     linkName: "Facture",
+//     link: "/invoices",
+//     icon: (<BsFillFileEarmarkRuledFill />) as IconType,
+//   },
+//   {
+//     linkName: "Posts",
+//     link: "/posts",
+//     icon: (<BsFillHddRackFill />) as IconType,
+//   },
+// ];
+
+// export default menuData;
