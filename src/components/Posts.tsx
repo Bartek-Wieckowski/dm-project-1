f33b794423cac4b1fb63e898d08580078b1907d0
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-17 21:19:57 +0100

    refactoring code and use useAPI custom hook

--- src/components/Posts.tsx
+++ src/components/Posts.tsx
@@ -1,46 +1,19 @@
-import { useEffect, useState } from 'react';
-import { Post as PostType } from '../types/Post.type';
+import { useState } from 'react';
 import { BsChevronRight, BsChevronLeft } from 'react-icons/bs';
 import Post from './Post';
+import useAPI from '../hooks/useAPI';
 
 const BASE_URL = 'https://jsonplaceholder.typicode.com/posts';
 const POSTS_PER_PAGE = 4;
 
 export default function Posts() {
-  const [posts, setPosts] = useState<PostType[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
-
-  useEffect(() => {
-    const fetchPosts = async () => {
-      try {
-        setIsLoading(true);
-        const res = await fetch(BASE_URL);
-        if (!res.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const data = (await res.json()) as PostType[];
-        setPosts(data);
-      } catch (error) {
-        setIsError(true);
-        console.error(error);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-
-    // fetchPosts();
-
-    void fetchPosts();
-  }, []);
+  const { posts, isError, isLoading } = useAPI(BASE_URL);
 
   const startIndex = (currentPage - 1) * POSTS_PER_PAGE;
   const endIndex = startIndex + POSTS_PER_PAGE;
   const slicedPosts = posts.slice(startIndex, endIndex);
   const totalPages = Math.ceil(posts.length / POSTS_PER_PAGE);
-
-  console.log(slicedPosts);
 
   const handlePrevClick = () => {
     setCurrentPage((prevPage) => Math.max(1, prevPage - 1));
