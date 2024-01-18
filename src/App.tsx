2840e9434fae71a9ef87f506e2b5aa044628c370
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-18 22:21:27 +0100

    add route to show exercise

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Postspage from './pages/Postspage';
 import Commentspage from './pages/Commentspage';
 import NotFound from './pages/NotFound';
 import Example from './pages/Example';
+import Example2 from './pages/Example2';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
           <Route path="comments" element={<Commentspage />}></Route>
           <Route path="posts" element={<Postspage />}></Route>
           <Route path="modals-example" element={<Example />}></Route>
+          <Route path="table-example" element={<Example2 />}></Route>
           <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
