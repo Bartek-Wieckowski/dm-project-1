8b688659da511ea69c4d2aebf9858a71279c03bf
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-22 22:49:22 +0100

    add few tailwind class

--- src/pages/AppLayout.tsx
+++ src/pages/AppLayout.tsx
@@ -1,17 +1,17 @@
-import { Outlet } from "react-router";
-import { footerData } from "../assets/dummy-data/footer-data";
-import AsideMenu from "../components/AsideMenu";
-import Footer from "../components/Footer";
-import Wrapper from "../components/Wrapper";
+import { Outlet } from 'react-router';
+import { footerData } from '../assets/dummy-data/footer-data';
+import AsideMenu from '../components/AsideMenu';
+import Footer from '../components/Footer';
+import Wrapper from '../components/Wrapper';
 
 export default function AppLayout() {
   return (
     <main className="grid min-h-screen grid-cols-[100px_1fr] place-items-center bg-slate-900">
       <AsideMenu />
-      <div className="w-full py-4">
+      <div className="w-full px-2 py-4">
         <Outlet />
       </div>
-      <div className="col-start-2 w-full text-center self-end">
+      <div className="col-start-2 w-full self-end text-center">
         <Wrapper>
           <Footer footerData={footerData} />
         </Wrapper>
