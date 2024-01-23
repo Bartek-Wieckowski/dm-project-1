aef02ce5fd43341636a90b5e05d52f4b6e0eae84
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-23 12:28:46 +0100

    read the client's id, find  static data and pass it to props

--- src/components/Clients/ClientEdit.tsx
+++ src/components/Clients/ClientEdit.tsx
@@ -1,9 +1,14 @@
-import ClientForm from './ClientForm';
+import { useParams } from "react-router-dom";
+import ClientForm from "./ClientForm";
+import cards from "../../assets/dummy-data/cards-data";
 
 export default function ClientEdit() {
+  const { id } = useParams();
+  const editedClient = cards.find((card) => card.id === Number(id));
+
   return (
     <>
-      <ClientForm editForm={true} />
+      <ClientForm editForm={true} editValues={editedClient} />
     </>
   );
 }
