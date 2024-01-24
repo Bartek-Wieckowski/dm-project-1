863102fa00a2de0daee5605bb85a516ca9a2b659
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-24 21:41:51 +0100

    update code for implement crud options and fetch data form database, add Loader to better ux

--- src/components/Clients/ClientEdit.tsx
+++ src/components/Clients/ClientEdit.tsx
@@ -1,14 +1,48 @@
-import { useParams } from "react-router-dom";
-import ClientForm from "./ClientForm";
-import cards from "../../assets/dummy-data/cards-data";
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getSingleClient } from '../../services/ClientsService';
+import { CardProps } from '../../types/CardProps.type';
+import ClientForm from './ClientForm';
+import Loader from '../Loader';
 
 export default function ClientEdit() {
   const { id } = useParams();
-  const editedClient = cards.find((card) => card.id === Number(id));
+  const [editedClient, setEditedClient] = useState<CardProps>({
+    id: '',
+    name: '',
+    surname: '',
+    street: '',
+    postCode: '',
+    town: '',
+    phoneNumber: '',
+  });
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchSingleClient = async () => {
+      try {
+        setIsLoading(true);
+        const clientData: CardProps = await getSingleClient(String(id));
+        setEditedClient(clientData);
+      } catch (error) {
+        console.error('Błąd pobierania danych klienta:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchSingleClient().catch((error) => {
+      console.error('Błąd podczas fetchSingleClientData:', error);
+    });
+  }, [id]);
 
   return (
     <>
-      <ClientForm editForm={true} editValues={editedClient} />
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <ClientForm editForm={true} editValues={editedClient} />
+      )}
     </>
   );
 }
--- src/components/Clients/Clients.tsx
+++ src/components/Clients/Clients.tsx
@@ -1,9 +1,30 @@
-import CardList from "../Cards/CardList";
+import { useEffect, useState } from 'react';
+import { getAllClients } from '../../services/ClientsService';
+import { CardProps } from '../../types/CardProps.type';
+import CardList from '../Cards/CardList';
+import Loader from '../Loader';
 
 export default function Clients() {
-  return (
-    <>
-      <CardList />
-    </>
-  );
+  const [clients, setClients] = useState<CardProps[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data: CardProps[] = await getAllClients();
+        setClients(data);
+      } catch (error) {
+        console.error('Błąd ładowania danych');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData().catch((error) => {
+      console.error('Error during fetchData:', error);
+    });
+  }, []);
+
+  return <>{isLoading ? <Loader /> : <CardList cards={clients} />}</>;
 }
