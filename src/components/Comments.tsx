6a1c27faf76f6666965efd8dc5f7663916080237
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-25 13:25:47 +0100

    add open/close each post if has subcomments

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,20 +1,33 @@
-import { Comment, CommentsProps } from '../types/CommentData.type';
-import { BsArrow90DegDown } from 'react-icons/bs';
+import { useState } from "react";
+import { Comment, CommentsProps } from "../types/CommentData.type";
+import { BsArrow90DegDown, BsArrow90DegUp } from "react-icons/bs";
 
-export default function Comments({
-  comments,
-}: CommentsProps): React.ReactElement {
+export default function Comments({ comments }: CommentsProps): React.ReactElement {
+  const [openStates, setOpenStates] = useState<{ [key: string]: boolean }>({});
+
+  const handleToggle = (username: string) => {
+    setOpenStates((prev) => ({
+      ...prev,
+      [username]: !prev[username],
+    }));
+  };
   return (
     <div className="w-full space-y-4 text-stone-200 sm:pl-8">
       {comments.map((comment: Comment) => (
         <div key={comment.username} className="rounded border p-4 shadow-md">
           <div className="flex items-center gap-4">
             <h2 className="mb-2 text-lg font-bold">{comment.username}</h2>
-            {comment.subComments && <BsArrow90DegDown />}
+            {comment.subComments && (
+              <div onClick={() => handleToggle(comment.username)} style={{ cursor: "pointer" }}>
+                {openStates[comment.username] ? <BsArrow90DegUp /> : <BsArrow90DegDown />}
+              </div>
+            )}
           </div>
           <p className="mb-4">{comment.comment}</p>
           <div>
-            {comment.subComments && <Comments comments={comment.subComments} />}
+            {comment.subComments && openStates[comment.username] && (
+              <Comments comments={comment.subComments} />
+            )}
           </div>
         </div>
       ))}
