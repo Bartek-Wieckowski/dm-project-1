e76c9b6cebd30ff821d67c338d551394dbcb13de
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-28 23:02:02 +0100

    updates(data,syntax prettier) files

--- src/api/apiClients.ts
+++ src/api/apiClients.ts
@@ -1,11 +1,11 @@
-import { ClientFormValues } from "../validators/validators";
-import { ClientProps } from "../types/ClientProps.type";
-import { API_URL } from "../constants/appConst";
+import { ClientFormValues } from '../validators/validators';
+import { ClientProps } from '../types/ClientProps.type';
+import { API_URL } from '../constants/appConst';
 
 export async function getAllClients(): Promise<ClientProps[]> {
   const res = await fetch(`${API_URL}/clients`);
   if (!res.ok) {
-    throw new Error("Błąd ładowania danych...");
+    throw new Error('Błąd ładowania danych...');
   }
   const data = (await res.json()) as ClientProps[];
   return data;
@@ -14,20 +14,22 @@ export async function getAllClients(): Promise<ClientProps[]> {
 export async function getSingleClient(clientId: string) {
   const res = await fetch(`${API_URL}/clients/${clientId}`);
   if (!res.ok) {
-    throw new Error("Błąd ładowania danych...");
+    throw new Error('Błąd ładowania danych...');
   }
   const data = (await res.json()) as ClientProps;
   return data;
 }
 
-export async function addClient(newClient: ClientFormValues): Promise<ClientProps> {
+export async function addClient(
+  newClient: ClientFormValues
+): Promise<ClientProps> {
   const res = await fetch(`${API_URL}/clients`, {
-    method: "POST",
-    headers: { "Content-type": "application/json" },
+    method: 'POST',
+    headers: { 'Content-type': 'application/json' },
     body: JSON.stringify(newClient),
   });
   if (!res.ok) {
-    throw new Error("Błąd podczas dodawania...");
+    throw new Error('Błąd podczas dodawania...');
   }
   const data = (await res.json()) as ClientProps;
   return data;
@@ -35,15 +37,15 @@ export async function addClient(newClient: ClientFormValues): Promise<ClientProp
 
 export async function updateClientById(
   updateClientData: ClientFormValues,
-  clientId: ClientProps["id"]
+  clientId: ClientProps['id']
 ): Promise<ClientProps> {
   const res = await fetch(`${API_URL}/clients/${clientId}`, {
-    method: "PATCH",
-    headers: { "Content-type": "application/json" },
+    method: 'PATCH',
+    headers: { 'Content-type': 'application/json' },
     body: JSON.stringify(updateClientData),
   });
   if (!res.ok) {
-    throw new Error("Błąd podczas aktualizacji...");
+    throw new Error('Błąd podczas aktualizacji...');
   }
   const data = (await res.json()) as ClientProps;
   return data;
@@ -51,9 +53,9 @@ export async function updateClientById(
 
 export async function deleteClient(clientId: string): Promise<void> {
   const res = await fetch(`${API_URL}/clients/${clientId}`, {
-    method: "DELETE",
+    method: 'DELETE',
   });
   if (!res.ok) {
-    throw new Error("Błąd podczas usuwania...");
+    throw new Error('Błąd podczas usuwania...');
   }
 }
