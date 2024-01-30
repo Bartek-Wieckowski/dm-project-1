801c859a07dc44892bedfd04c102c9c0ff74e7cd
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-30 20:39:29 +0100

    create Protected component

---
+++ src/components/ProtectedWrapper.tsx
@@ -0,0 +1,17 @@
+import { useNavigate } from "react-router-dom";
+import { useUser } from "../contexts/UserContext";
+import { useEffect } from "react";
+
+export default function ProtectedWrapper({ children }: { children: React.ReactNode }) {
+  const navigate = useNavigate();
+  const {
+    userData: { isAuth },
+  } = useUser();
+
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/");
+    }
+  }, [isAuth]);
+  if (isAuth) return children;
+}
