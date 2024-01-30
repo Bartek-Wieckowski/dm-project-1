80068fae7e8d81e075f7525b87ad62ef16366db0
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-30 19:59:13 +0100

    create avatar component

---
+++ src/components/Avatar.tsx
@@ -0,0 +1,18 @@
+import { RANDOM_IMG_URL } from "../constants/appConst";
+import { useUser } from "../contexts/UserContext";
+
+export default function Avatar() {
+  const {
+    userData: { user },
+  } = useUser();
+  return (
+    <div className="flex flex-col items-center justify-center gap-3">
+      <img
+        className="w-10 h-10 p-1 rounded-full ring-2 ring-gray-300 dark:ring-gray-500"
+        src={user?.avatar ? user.avatar : RANDOM_IMG_URL}
+        alt="Bordered avatar"
+      />
+      <small className="text-stone-200">{user?.username}</small>
+    </div>
+  );
+}
