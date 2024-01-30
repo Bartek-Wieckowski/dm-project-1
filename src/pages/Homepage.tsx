12514e12e3f65ad78bdb59ab91d10ca0eb99f169
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-30 19:55:11 +0100

    display different content when user is log or not

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,9 +1,19 @@
+import Button from "../components/Button";
 import Login from "../components/Login/Login";
+import { useUser } from "../contexts/UserContext";
 
 export default function Homepage() {
+  const {
+    userData: { isAuth, user },
+  } = useUser();
   return (
     <>
-      <Login />
+      {!isAuth && <Login />}
+      {isAuth && (
+        <div className="flex items-center gap-4 justify-center">
+          <h1 className="text-stone-200 text-center text-5xl">Witaj {user?.username}</h1>
+        </div>
+      )}
     </>
   );
 }
