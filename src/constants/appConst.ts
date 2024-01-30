9e769d94dcdb32c7faaaf55fc30ce075c7953058
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-30 19:59:22 +0100

    update files

--- src/constants/appConst.ts
+++ src/constants/appConst.ts
@@ -1,1 +1,4 @@
 export const API_URL = "http://localhost:8000";
+
+export const RANDOM_IMG_URL =
+  "https://w7.pngwing.com/pngs/574/369/png-transparent-avatar-computer-icons-user-random-icons-purple-blue-heroes.png";
