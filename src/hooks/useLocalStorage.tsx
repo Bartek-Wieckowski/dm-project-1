02aaf6686ea1e8152f2b1af839963b46d53706dc
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-31 19:03:11 +0100

    create localStorage hook

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,16 @@
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
+
+type UseLocalStorageType<T> = [T, Dispatch<SetStateAction<T>>];
+
+export function useLocalStorage<T>(initialState: T, key: string): UseLocalStorageType<T> {
+  const [value, setValue] = useState<T>(() => {
+    const storedValue = localStorage.getItem(key);
+    return storedValue ? JSON.parse(storedValue) : initialState;
+  });
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value, key]);
+
+  return [value, setValue];
+}
