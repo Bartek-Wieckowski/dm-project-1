680caf2949c91e5b079878a4b3c394c2e65117e0
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-01-31 14:22:46 +0100

    use Modal to display question in delete action

--- src/components/Clients/ClientDetails.tsx
+++ src/components/Clients/ClientDetails.tsx
@@ -1,11 +1,12 @@
-import { useNavigate, useParams } from 'react-router-dom';
-import { useClientDetails } from './useClientDetails';
-import { useClientDelete } from './useClientDelete';
-import Button from '../Button';
-import Loader from '../Loader';
-import TableRow from '../Tables/TableRow';
-import TableTh from '../Tables/TableTh';
-import TableTd from '../Tables/TableTd';
+import { useNavigate, useParams } from "react-router-dom";
+import { useClientDetails } from "./useClientDetails";
+import { useClientDelete } from "./useClientDelete";
+import Button from "../Button";
+import Loader from "../Loader";
+import TableRow from "../Tables/TableRow";
+import TableTh from "../Tables/TableTh";
+import TableTd from "../Tables/TableTd";
+import Modal from "../../contexts/ModalContext";
 
 type ParamsType = {
   id?: string;
@@ -14,11 +15,7 @@ type ParamsType = {
 export default function ClientDetails() {
   const { id } = useParams<ParamsType>();
   const navigate = useNavigate();
-  const {
-    isLoading,
-    clientDetails: client,
-    error: clientNotFound,
-  } = useClientDetails(id || '');
+  const { isLoading, clientDetails: client, error: clientNotFound } = useClientDetails(id || "");
   const { deleteClient } = useClientDelete();
 
   if (isLoading) {
@@ -26,67 +23,54 @@ export default function ClientDetails() {
   }
 
   if (clientNotFound) {
-    return (
-      <div className="text-center text-5xl text-stone-200">
-        Nie znaleziono klienta o ID: {id}
-      </div>
-    );
+    return <div className="text-center text-5xl text-stone-200">Nie znaleziono klienta o ID: {id}</div>;
   }
 
   if (!client) {
     return <></>;
   }
 
-  const {
-    id: clientId,
-    imgSrc,
-    name,
-    surname,
-    street,
-    postCode,
-    town,
-    subRegion,
-    phoneNumber,
-  } = client;
+  const { id: clientId, imgSrc, name, surname, street, postCode, town, subRegion, phoneNumber } = client;
 
-  const handleClickDelete = () => {
-    const confirmDelete = window.confirm(
-      'Czy na pewno chcesz usunąć tego klienta?'
-    );
-
-    if (confirmDelete) {
-      deleteClient(clientId);
-      navigate('/clients');
-    }
-  };
-
-  const fieldsTh = [{ label: 'Kategoria' }, { label: 'Dane' }];
+  const fieldsTh = [{ label: "Kategoria" }, { label: "Dane" }];
 
   const fields = [
-    { label: 'Imie', value: name },
-    { label: 'Nazwisko', value: surname },
-    { label: 'Ulica', value: street },
-    { label: 'Kod pocztowy', value: postCode },
-    { label: 'Miasto', value: town },
-    { label: 'Region', value: subRegion },
+    { label: "Imie", value: name },
+    { label: "Nazwisko", value: surname },
+    { label: "Ulica", value: street },
+    { label: "Kod pocztowy", value: postCode },
+    { label: "Miasto", value: town },
+    { label: "Region", value: subRegion },
     {
-      label: 'Zdjęcie',
+      label: "Zdjęcie",
       value: <img src={imgSrc} alt={name} className="size-1/2 rounded" />,
     },
-    { label: 'Numer telefonu', value: phoneNumber },
+    { label: "Numer telefonu", value: phoneNumber },
   ];
 
   return (
     <div className="flex flex-col items-center">
       <div className="p-3">
-        <h3 className="text-center text-5xl text-stone-200">
-          Klient: {clientId}{' '}
-        </h3>
+        <h3 className="text-center text-5xl text-stone-200">Klient: {clientId} </h3>
       </div>
       <div className="flex gap-4 p-3">
-        <Button type="button" btnStyles="btnDelete" onClick={handleClickDelete}>
-          Usuń
-        </Button>
+        <Modal>
+          <Modal.Open opensWindowName="deleteConfirmation">
+            <Button type="button" btnStyles="btnDelete">
+              Usuń
+            </Button>
+          </Modal.Open>
+          <Modal.Window
+            name="deleteConfirmation"
+            clickOk={() => {
+              deleteClient(clientId);
+              navigate("/clients");
+            }}
+            showButtonOk={true}
+          >
+            <div className="text-center">Czy na pewno chcesz usunąć tego klienta?</div>
+          </Modal.Window>
+        </Modal>
         <Button to={`/clients/${clientId}/edit`} btnStyles="btnEdit">
           Edytuj
         </Button>
@@ -97,12 +81,7 @@ export default function ClientDetails() {
           <thead className="text-xs uppercase text-gray-900 dark:text-gray-400">
             <TableRow>
               {fieldsTh.map((fieldTh, index) => (
-                <TableTh
-                  key={index}
-                  scope="col"
-                  label={fieldTh.label}
-                  className="px-6 py-3"
-                />
+                <TableTh key={index} scope="col" label={fieldTh.label} className="px-6 py-3" />
               ))}
             </TableRow>
           </thead>
@@ -110,11 +89,9 @@ export default function ClientDetails() {
             {fields.map((field, index) => (
               <TableRow className="bg-gray-800" key={index}>
                 <TableTh
-                  scope={'row'}
+                  scope={"row"}
                   label={field.label}
-                  className={
-                    'whitespace-nowrap px-6 py-4 font-medium text-white'
-                  }
+                  className={"whitespace-nowrap px-6 py-4 font-medium text-white"}
                 />
                 <TableTd value={field.value} className="px-3 py-6" />
               </TableRow>
