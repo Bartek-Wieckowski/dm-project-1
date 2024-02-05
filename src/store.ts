09e96f4d54199a598316033cc9c775eeea82beef
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-05 13:08:09 +0100

    create order reducer

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import moneyReducer from './redux/money/moneySlice';
+import orderReducer from './redux/order/orderSlice';
 
 const store = configureStore({
   reducer: {
     money: moneyReducer,
+    order: orderReducer,
   },
 });
 
