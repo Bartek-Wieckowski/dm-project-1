08506ce3c170b8752063a301de86d0a1c2d988d3
Bartek Wieckowski <bart.it.pro@gmail.com>
2024-02-07 13:52:47 +0100

    export type

--- src/types/Invoice.types.ts
+++ src/types/Invoice.types.ts
@@ -1,3 +1,9 @@
+import { FormikProps } from 'formik';
 import { InvoicesFormValues } from '../validators/validators';
 
 export type InvoiceData = InvoicesFormValues & { id: string };
+
+
+export type InvoiceFormikProps = {
+  formik: FormikProps<InvoicesFormValues>;
+};
